e68c9410c1fd38a41754a6625a96ebfda74ae167
Nonpoint93 <jesusj_t93@hotmail.com>
2025-01-13 21:01:16 +0000

    #logist_regresion - refactored code

--- src/bank_marketing/bank_service.py
+++ src/bank_marketing/bank_service.py
@@ -1,7 +1,57 @@
 import pandas as pd
 import sys
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.discriminant_analysis import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.model_selection import GridSearchCV
+import warnings
+
+"""
+
+Description of the problem
+
+The Portuguese bank is experiencing a decline in revenue, so they want to be able to identify existing customers who are more likely to take out a long-term deposit. This will allow the bank to focus their marketing efforts on those customers and avoid wasting money and time on customers who are unlikely to sign up.
+
+To address this problem we will create a ranking algorithm to help predict whether or not a customer will sign up for a long-term deposit.
+
+
+    age. Age of customer (numeric)
+    job. Type of job (categorical)
+    marital. Marital status (categorical)
+    education. Level of education (categorical)
+    default. Do you currently have credit (categorical)
+    housing. Do you have a housing loan (categorical)
+    loan. Do you have a personal loan? (categorical)
+    contact. Type of contact communication (categorical)
+    month. Last month in which you have been contacted (categorical)
+    day_of_week. Last day on which you have been contacted (categorical)
+    duration. Duration of previous contact in seconds (numeric)
+    campaign. Number of contacts made during this campaign to the customer (numeric)
+    pdays. Number of days that elapsed since the last campaign until the customer was contacted (numeric)
+    previous. Number of contacts made during the previous campaign to the customer (numeric)
+    poutcome. Result of the previous marketing campaign (categorical)
+    emp.var.rate. Employment variation rate. Quarterly indicator (numeric)
+    cons.price.idx. Consumer price index. Monthly indicator (numeric)
+    cons.conf.idx. Consumer confidence index. Monthly indicator (numeric)
+    euribor3m. EURIBOR 3-month rate. Daily indicator (numeric)
+    nr.employed. Number of employees. Quarterly indicator (numeric)
+    y. TARGET. Whether the customer takes out a long-term deposit or not (categorical)
+
+"""
 
 class BankService:
+
+    hyperparams = {
+	    "C": [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+	    "penalty": ["l1", "l2", "elasticnet", "none"],
+	    "solver": ["newton-cg", "lbfgs", "liblinear", "sag", "saga"],
+        "max_iter": [200, 250, 300]
+	}
+
+
     def __init__(self):
         pass
 
@@ -24,8 +74,71 @@ class BankService:
         print(dataframe.info())
         print(dataframe.head())
         print(dataframe.shape)
+        print(dataframe.describe())
 
+    def display_correlation_dataset(self, dataframe: pd.DataFrame):
+        
+        """ Displaying correlation dataset """
+
+        sns.heatmap(dataframe.corr(numeric_only=True), annot=True, cmap='coolwarm', fmt=".2f")
+
+        plt.title("Correlation Matrix")
+        plt.tight_layout()
+        plt.show()
+
+    def remove_outliers(self, df, column) -> pd.DataFrame:
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
+        df = df[~((df[column] < (Q1 - 1.5 * IQR)) | (df[column] > (Q3 + 1.5 * IQR)))]
+        return df
+    
+
+    def warn(self, *args, **kwargs):
+        pass
 
     def process_data(self):
         dataframe = self.read_dataset()
         self.display_info_dataset(dataframe=dataframe)
+
+        dataframe.loc[dataframe['default'] == 'unknown', 'default'] = 'no'
+        dataframe["y"] = dataframe['y'].apply(lambda x: 1 if x == 'yes' else 0)
+        dataframe["default"] = dataframe["default"].apply(lambda x: 1 if x == 'yes' else 0)
+        dataframe["loan"] = dataframe["loan"].apply(lambda x: 1 if x == 'yes' else 0)
+        dataframe["housing"] = dataframe["housing"].apply(lambda x: 1 if x == 'yes' else 0)
+
+        # Codificación de variables categóricas
+        dataframe = pd.get_dummies(dataframe, columns=['job', 'marital', 'education', 'contact', 'month', 'day_of_week', 'poutcome'])
+
+        # Normalización de características numéricas
+        numeric_features = dataframe.select_dtypes(include=["int64", "float64"]).columns
+        scaler = StandardScaler()
+        dataframe[numeric_features] = scaler.fit_transform(dataframe[numeric_features])
+
+        X = dataframe.drop("y", axis=1)
+        y = dataframe["y"]
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42, train_size = 0.80)
+
+        model = LogisticRegression(class_weight='balanced')
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        print(classification_report(y_test, y_pred))
+        print(f"ROC-AUC score: {roc_auc_score(y_test, model.predict_proba(X_test)[:, 1])}")
+
+
+        compatible_hyperparams = [
+            (C, penalty, solver, max_iter)
+            for C in self.hyperparams["C"]
+            for penalty in self.hyperparams["penalty"]
+            for solver in self.hyperparams["solver"]
+            for max_iter in self.hyperparams["max_iter"]
+            if (solver in ["liblinear"] and penalty in ["l1", "l2"]) or
+            (solver in ["sag", "saga", "lbfgs"] and penalty in ["l2", "none"]) or
+            (solver == "saga" and penalty in ["l1", "elasticnet", "l2"])
+        ]
+        
+        grid = GridSearchCV(model, self.hyperparams, scoring = "accuracy", cv = 5)
+        grid.fit(X_train, y_train)
+        print(f"Best hyperparamters: {grid.best_params_}")
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
+import pandas as pd
 
 # load the .env file variables
 load_dotenv()
@@ -9,3 +10,14 @@ def db_connect():
     engine = create_engine(os.getenv('DATABASE_URL'))
     engine.connect()
     return engine
+
+
+
+def remove_outliers(dataframe, column) -> pd.DataFrame:
+    """ Eliminar outliers """
+    q1 = dataframe[column].quantile(0.25)
+    q3 = dataframe[column].quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    return dataframe[(dataframe[column] >= lower_bound) & (dataframe[column] <= upper_bound)]
